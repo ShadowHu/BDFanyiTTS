3331bf64b2ab692405e789a5b6697157b1c6b5ee
ShadowHu <shadow_hu1441@163.com>
2018-05-04 19:29:23 +0800

    Now support a text file input

--- getTTS.py
+++ getTTS.py
@@ -7,6 +7,16 @@
 import requests
 import os
 
+
+def reqmp3(url, lan, text, mp3dir, dirname):
+    url = url + "lan=" + lan + "&text=" + text + "&spd=3&source=web"
+    req = requests.get(url)
+    if not os.path.exists(os.path.join(mp3dir, dirname)):
+        os.mkdir(os.path.join(mp3dir, dirname))
+    with open(os.path.join(mp3dir, dirname, text + '.mp3'), 'wb') as file:
+        file.write(req.content)
+
+
 url = "http://fanyi.baidu.com/gettts?"
 cwd = os.getcwd()
 mp3dir = os.path.join(cwd, 'Mp3files')
@@ -15,15 +25,19 @@ if not os.path.exists(mp3dir):
     os.mkdir(mp3dir)
 
 lannum = input("Which language? (1.English; 2.Japanese)\n")
-text = input("Input your text: \n")
+text = input("Input your text:(If you have a text file just click 'Enter') \n")
+
 
 if lannum == '1':
     lan = 'en'
 elif lannum == '2':
     lan = 'jp'
 
-url = url + "lan=" + lan + "&text=" + text + "&spd=3&source=web"
+if text:
+    reqmp3(url, lan, text, mp3dir, 'SingleText')
+else:
+    textfile = input("Or drag your text file below.\n")
+    with open(textfile) as tfile:
+        for text in tfile:
+            reqmp3(url, lan, text.rstrip(), mp3dir, os.path.split(textfile)[-1][:-4])
 
-req = requests.get(url)
-with open(os.path.join(mp3dir, text + '.mp3'), 'wb') as file:
-    file.write(req.content)
